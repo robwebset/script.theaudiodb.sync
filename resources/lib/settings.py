ba85cb98ce2d7499537f5c84ed755ed3ea440efe
robwebset <rob@deadlyduck.com>
2016-12-23 14:28:56 +0000

    TheAudioDB

---
+++ resources/lib/settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import xbmc
+import xbmcaddon
+
+ADDON = xbmcaddon.Addon(id='script.theaudiodb')
+ADDON_ID = ADDON.getAddonInfo('id')
+
+
+# Common logging module
+def log(txt, loglevel=xbmc.LOGDEBUG):
+    if (ADDON.getSetting("logEnabled") == "true") or (loglevel != xbmc.LOGDEBUG):
+        if isinstance(txt, str):
+            txt = txt.decode("utf-8")
+        message = u'%s: %s' % (ADDON_ID, txt)
+        xbmc.log(msg=message.encode("utf-8"), level=loglevel)
+
+
+##############################
+# Stores Various Settings
+##############################
+class Settings():
+    @staticmethod
+    def getUsername():
+        return ADDON.getSetting("username")
