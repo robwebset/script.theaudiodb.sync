61a505b76ca0f1e87cb8c2f4ad9260a89ac807cd
robwebset <rob@deadlyduck.com>
2017-01-16 21:52:49 +0000

    Add option to schedule synchronisation

--- default.py
+++ default.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
+import time
 import xbmcgui
 import xbmcaddon
 
 # Import the common settings
 from resources.lib.settings import log
 from resources.lib.settings import Settings
-from resources.lib.theaudiodb import TheAudioDb
-from resources.lib.library import MusicLibrary
+from resources.lib.sync import LibrarySync
 
 
 ADDON = xbmcaddon.Addon(id='script.theaudiodb.sync')
@@ -25,84 +25,23 @@ if __name__ == '__main__':
         # Show a dialog detailing that the username is not set
         xbmcgui.Dialog().ok(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32005))
     else:
-        theAudioDb = TheAudioDb(username)
-        musicLib = MusicLibrary()
+        performResync = True
+        # Before performing the resync, check when the last time a resync was done, we want
+        # to try and discourage people doing too many resyncs in quick succession
+        lastResyncTimeStr = Settings.getLastSyncTime()
+        if lastResyncTimeStr not in [None, ""]:
+            currentTime = int(time.time())
+            # check if the last resync was within 5 minuted
+            if currentTime < (int(lastResyncTimeStr) + 300):
+                performResync = xbmcgui.Dialog().yesno(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32016))
 
-        # Counters for the amount of data updated
-        numTracksUpdated = 0
-        numAlbumsUpdated = 0
+        if performResync:
+            # Perform the resync operation and display the status
+            numAlbumsUpdated, numTracksUpdated = LibrarySync.syncToLibrary(username, True)
 
-        progressDialog = xbmcgui.DialogProgressBG()
-        try:
-            progressDialog.create(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32007))
-
-            # Unfortunately we can not filter based on the musicbrainztrackid as that is not a
-            # valid filter support this means we just have to get all tracks
-            libraryTracks = []
-            if Settings.isUpdateTrackRatings():
-                libraryTracks = musicLib.getLibraryTracks()
-
-            # Set the percentage that it takes to get all the tracks from the library
-            # at about 5% for now
-            currentPercent = float(5)
-
-            # Now that we have an idea of the number of tracks that there are to
-            # process we can give a better idea of the progress
-            displayMsg = "%d %s" % (len(libraryTracks), ADDON.getLocalizedString(32008))
-            progressDialog.update(percent=int(currentPercent), message=displayMsg)
-
-            # Now get the album details
-            libraryAlbums = []
-            if Settings.isUpdateAlbumRatings():
-                libraryAlbums = musicLib.getLibraryAlbums()
-
-            # Set the percentage that it takes to get all the albums from the library
-            # at about 5% for now
-            currentPercent = float(10)
-
-            # Calculate the percentage progress that each track will be
-            perItemPercent = float((float(len(libraryTracks) + len(libraryAlbums))) / float(90))
-
-            if len(libraryTracks) > 0:
-                # For each rating, check to see if the track appears in the library
-                for libTrack in libraryTracks:
-                    # Get the ratings for this track
-                    rating, totalRating = theAudioDb.getRatingForTrack(libTrack)
-                    # Perform the library update for this track
-                    trackUpdated = musicLib.updateLibraryTrackRatings(libTrack, rating, totalRating)
-                    if trackUpdated:
-                        numTracksUpdated = numTracksUpdated + 1
-                    currentPercent = float(currentPercent + perItemPercent)
-                    progressDialog.update(percent=int(currentPercent))
-
-            if len(libraryAlbums) > 0:
-                # Set the progress bar to state that we are processing the albums
-                displayMsg = "%d %s" % (len(libraryAlbums), ADDON.getLocalizedString(32009))
-                progressDialog.update(percent=int(currentPercent), message=displayMsg)
-
-                # Process the albums
-                for libAlbum in libraryAlbums:
-                    # Get the ratings for this album
-                    rating, totalRating = theAudioDb.getRatingForAlbum(libAlbum)
-                    # Perform the library update for this album
-                    albumUpdated = musicLib.updateLibraryAlbumRatings(libAlbum, rating, totalRating)
-                    if albumUpdated:
-                        numAlbumsUpdated = numAlbumsUpdated + 1
-                    currentPercent = float(currentPercent + perItemPercent)
-                    progressDialog.update(percent=int(currentPercent))
-
-        finally:
-            progressDialog.close()
-
-        # Store the time that the resync was last performed
-        Settings.setLastSyncTime()
-
-        # Display a summary of what was performed
-        summaryAlbums = "%d %s" % (numAlbumsUpdated, ADDON.getLocalizedString(32010))
-        summaryTracks = "%d %s" % (numTracksUpdated, ADDON.getLocalizedString(32011))
-        xbmcgui.Dialog().ok(ADDON.getLocalizedString(32001), summaryAlbums, summaryTracks)
-
-        del musicLib
-        del theAudioDb
+            # Display a summary of what was performed
+            summaryAlbums = "%d %s" % (numAlbumsUpdated, ADDON.getLocalizedString(32010))
+            summaryTracks = "%d %s" % (numTracksUpdated, ADDON.getLocalizedString(32011))
+            xbmcgui.Dialog().ok(ADDON.getLocalizedString(32001), summaryAlbums, summaryTracks)
 
     log("TheAudioDBSync Finished")
---
+++ service.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import time
+import xbmcaddon
+
+# Import the common settings
+from resources.lib.settings import log
+from resources.lib.settings import Settings
+from resources.lib.sync import LibrarySync
+
+
+ADDON = xbmcaddon.Addon(id='script.theaudiodb.sync')
+
+
+##################################
+# Main of TheAudioDBSync Script
+##################################
+if __name__ == '__main__':
+    log("TheAudioDBSync Service Starting %s" % ADDON.getAddonInfo('version'))
+
+    # Get the username
+    username = Settings.getUsername()
+
+    # If the username is not set, then nothing to do yet
+    if username not in [None, ""]:
+        performResync = False
+        nextResyncTime = Settings.getNextScheduledResyncTime()
+
+        if nextResyncTime not in [None, "", "0"]:
+            log("Service: Next Sync time is %d" % nextResyncTime)
+            currentTime = int(time.time())
+            log("Service: Current time is %d" % currentTime)
+            # check if the last resync was within 5 minuted
+            if currentTime > nextResyncTime:
+                performResync = True
+
+        if performResync:
+            # Check if the status should be displayed
+            displayProgress = Settings.isScheduleDisplayProgress()
+            # Perform the resync operation and display the status
+            numAlbumsUpdated, numTracksUpdated = LibrarySync.syncToLibrary(username, displayProgress)
+
+    log("TheAudioDBSync Service Finished")
